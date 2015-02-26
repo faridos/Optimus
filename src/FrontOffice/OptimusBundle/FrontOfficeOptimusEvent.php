f728a98b2dcfd69198523d80f9598bb1c9a656aa
samirchandoul <samir.chandoul@gmail.com>
2015-02-26 10:19:10 +0100

    listner

--- src/FrontOffice/OptimusBundle/FrontOfficeOptimusEvent.php
+++ src/FrontOffice/OptimusBundle/FrontOfficeOptimusEvent.php
@@ -8,7 +8,7 @@ class FrontOfficeOptimusEvent {
    const AFTER_CLUB_REGISTER = "front_event.after_club_register";
    const PARICIAPTION_REGISTER = "front_event.participation_register";
    const NOTIFICATION_CLUB = "front_event.notification_club";
-   
+   const NOTIFICATION_SEEN_USER = "front_event.notification_seen_user";
    
     
 }
