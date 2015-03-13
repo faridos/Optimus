9cbca6383f731eb90bde3f481bda3b0a2fc912f7
bskhaled <benslamakhaled@gmail.com>
2015-03-13 18:00:24 +0100

    socialshare

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -25,6 +25,7 @@ class AppKernel extends Kernel
             new DCS\RatingBundle\DCSRatingBundle(),
             new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
             new Sly\RelationBundle\SlyRelationBundle(),
+            new Nomaya\SocialBundle\NomayaSocialBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
