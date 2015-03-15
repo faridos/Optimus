b1b04bd7de0bc07adcf585ad52f0731267e70a4a
samirchandoul <samir.chandoul@gmail.com>
2015-03-15 10:14:11 +0100

    resetting

--- src/FrontOffice/UserBundle/Controller/ResettingController.php
+++ src/FrontOffice/UserBundle/Controller/ResettingController.php
@@ -29,65 +29,6 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
  */
 class ResettingController extends BaseController
 {
-    /**
-     * Request reset user password: show form
-     */
-    public function requestAction()
-    {
-        return $this->render('FOSUserBundle:Resetting:request.html.twig');
-    }
-
-    /**
-     * Request reset user password: submit form and send email
-     */
-    public function sendEmailAction(Request $request)
-    {
-        $username = $request->request->get('username');
-
-        /** @var $user UserInterface */
-        $user = $this->get('fos_user.user_manager')->findUserByUsernameOrEmail($username);
-
-        if (null === $user) {
-            return $this->render('FOSUserBundle:Resetting:request.html.twig', array(
-                'invalid_username' => $username
-            ));
-        }
-
-        if ($user->isPasswordRequestNonExpired($this->container->getParameter('fos_user.resetting.token_ttl'))) {
-            return $this->render('FOSUserBundle:Resetting:passwordAlreadyRequested.html.twig');
-        }
-
-        if (null === $user->getConfirmationToken()) {
-            /** @var $tokenGenerator \FOS\UserBundle\Util\TokenGeneratorInterface */
-            $tokenGenerator = $this->get('fos_user.util.token_generator');
-            $user->setConfirmationToken($tokenGenerator->generateToken());
-        }
-
-        $this->get('fos_user.mailer')->sendResettingEmailMessage($user);
-        $user->setPasswordRequestedAt(new \DateTime());
-        $this->get('fos_user.user_manager')->updateUser($user);
-
-        return new RedirectResponse($this->generateUrl('fos_user_resetting_check_email',
-            array('email' => $this->getObfuscatedEmail($user))
-        ));
-    }
-
-    /**
-     * Tell the user to check his email provider
-     */
-    public function checkEmailAction(Request $request)
-    {
-        $email = $request->query->get('email');
-
-        if (empty($email)) {
-            // the user does not come from the sendEmail action
-            return new RedirectResponse($this->generateUrl('fos_user_resetting_request'));
-        }
-
-        return $this->render('FOSUserBundle:Resetting:checkEmail.html.twig', array(
-            'email' => $email,
-        ));
-    }
 
     /**
      * Reset user password
