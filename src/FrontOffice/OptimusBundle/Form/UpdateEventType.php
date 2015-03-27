9cf665e69df4e7fce97204d813efe520fe1c1df9
samirchandoul <samir.chandoul@gmail.com>
2015-03-27 12:18:12 +0100

    date

---
+++ src/FrontOffice/OptimusBundle/Form/UpdateEventType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace FrontOffice\OptimusBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class EventType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('titre','text',array('attr'=>array('class'=>'search-optimus','placeholder'=>'Entrer le titre')))
+            ->add('lieu','text',array('attr'=>array('class'=>'search-optimus','placeholder'=>'Entrer le lieu'),'required'=>True))
+            ->add('dateDebut','datetime', array('attr'=>array('class'=>'some_class search-optimus','placeholder'=>'Entrer la date de début'),'widget' => 'single_text','required' => true))
+            ->add('dateFin','datetime', array('attr'=>array('class'=>'some_class search-optimus','placeholder'=>'Entrer la date de fin'),'widget' => 'single_text','required' => true))
+            ->add('nbrPlaces','integer',array('attr'=>array('min'=>0,'class'=>'search-optimus','placeholder'=>'Entrer le nombre de places')))
+            ->add('frais','integer',array('attr'=>array('min'=>0,'class'=>'form-control round sansBorder','placeholder'=>'Entrer les frais d\'inscription')))
+            ->add('description','textarea',array('attr'=>array('class'=>'form-control search-optimus','placeholder'=>'Entrer la description du votre évènement...'),'required'=>false))
+           
+            ->add('type','text',array('attr'=>array('class'=>'search-optimus','placeholder'=>'Entrer le Type')))
+            ->add('lat','number',array('attr'=>array('class'=>'search-optimus')),array('required'=>true))
+            ->add('lng','number',array('attr'=>array('class'=>'search-optimus')),array('required'=>true))
+            
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'FrontOffice\OptimusBundle\Entity\Event'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'frontoffice_optimusbundle_event';
+    }
+}
